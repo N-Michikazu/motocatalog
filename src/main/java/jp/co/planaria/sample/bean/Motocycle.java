3630a6b2270e63341ad37f2e51124b130d60d7d2
michikazu0104 <m.nakayama@i-voice.co.jp>
2026-04-21 14:56:59 +0900

    動的ページ作成

---
+++ src/main/java/jp/co/planaria/sample/bean/Motocycle.java
@@ -0,0 +1,24 @@
+package jp.co.planaria.sample.bean;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+/* バイク */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Motocycle {
+    private int motoNo;
+    private String motoName;
+    private int  seatHeight;
+    private int cylinder;
+    private String cooling;
+    private int price;
+    private String comment;
+    private Brand brand;
+    private int version;
+    private LocalDateTime insDt;
+    private LocalDateTime updDt;
+}
